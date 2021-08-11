605bcd8ac8c682a1c52500f3d588609fea4df038
Abdiel Willyar Goni <85111427+AbdielWillyar@users.noreply.github.com>
2021-08-11 14:14:00 +0800

    Update Pertumbuhan_Penjualan.sql

--- Project Data Analysis for B2B retail: Customer Analytics Report/Pertumbuhan_Penjualan.sql
+++ Project Data Analysis for B2B retail: Customer Analytics Report/Pertumbuhan_Penjualan.sql
@@ -15,3 +15,35 @@ FROM
     orders_2
 WHERE
     status = "Shipped";
+  
+  
+  
+#Menghitung persentasi keseluruhan penjualan
+SELECT
+    quarter,
+    SUM(quantity) AS total_penjualan,
+    SUM(quantity * priceeach) AS revenue
+FROM
+    (
+        SELECT
+            orderNumber,
+            status,
+            quantity,
+            priceeach,
+            "1" as quarter
+        FROM
+            orders_1
+        UNION
+        SELECT
+            orderNumber,
+            status,
+            quantity,
+            priceeach,
+            "2" as quarter
+        FROM
+            orders_2
+    ) AS tabel_a
+WHERE
+    status = "Shipped"
+GROUP BY
+    quarter;
